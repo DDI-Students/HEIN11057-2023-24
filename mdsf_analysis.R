04304bd57d5f6853d7ca29f116d76875c2e60e27
Keith Douglas <keith@keithdouglas.co.uk>
2024-06-04 10:58:15 +0100

    Update mdsf_analysis.R
    Change data_path to file_path

--- mdsf_analysis.R
+++ mdsf_analysis.R
@@ -4,7 +4,7 @@ library(readxl)
 library(janitor)
 
 
-data_path <- './data/mdsf_2022-03-31.xlsx'
+file_path <- './data/mdsf_2022-03-31.xlsx'
 
 #| label: read-data
-mdsf_data <- read_xlsx(data_path, col_types = 'text')
+mdsf_data <- read_xlsx(file_path, col_types = 'text')
